2eba25ee204d4ae1bf7089df6fb35f2ddb752a9b
Rattawan Putnual <49346370+Gam1999@users.noreply.github.com>
2020-04-27 16:37:51 +0700

    Add files via upload
    Code pokemon Game

--- Pokemon.java
+++ Pokemon.java
@@ -1,43 +1,24 @@
-public class Pokemon{
-    private String  name;
-    private int level;
-    private int hp;
-    private int pp;
-
-    public Pokemon(){
-        this.name = "Charmender";
-        this.level = 1;
-        this.hp = 10;
-        this.pp = 10;
-    }
-
-    public void eatBerry(){
-        this.hp += 2;
-    }
-
-    public void sleep(){
-        this.hp += 2;
-    }
-
-    public void run(){
-        this.hp += 5;
-    }
-
-    public void attack(){
-        this.level += 1;
-        this.hp += 5;
-        this.pp += 5;
-    }
-
-    public void faint(){
-        this.hp = 0;
-    }
-
-    public void printStatus(){
-        System.out.println("Name= "+this.name);
-        System.out.println("Level= "+this.level);
-        System.out.println("HP= "+this.hp);
-        System.out.println("PP= "+this.pp);
-    }
+interface  Pokemon{
     
+    public int getMpEatBerry();
+    
+    public int getMp();
+    
+    public int getMpAttack();
+
+    public int getExp();
+
+    public int getExpZero();
+ 
+    public int  HP();
+   
+    public String getName();
+
+    public void setName(String name);
+  
+    public void setL(int hplevel);
+
+    public int getL();
+
+    public int getLevelUp();
 }
--- PokemonGame.java
+++ PokemonGame.java
@@ -1,19 +1,32 @@
-//Show results
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
 public class PokemonGame{
-    public static void main (String args[]){
-        Pokemon pokemon = new Pokemon();
-        System.out.println("//initial Status//");
-        pokemon.printStatus();
-
-        System.out.println("//update Status//");
-        pokemon.eatBerry();
-        pokemon.sleep();
-        pokemon.run();
-        pokemon.attack();
-        pokemon.faint();
-        pokemon.printStatus();
-    }
     
-
-
+    public static void main(String[] args){ 
+        //FormGUI
+        Form1 form = new Form1();
+	    form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        form.setSize(1000, 600); 
+        //image
+		ImagePanel panel = new ImagePanel(new ImageIcon("ProPokemon.jpg").getImage());
+        panel.setLayout(null);
+        panel.setBounds(120, 0, 950, 950);//x y w h
+    	form.getContentPane().add(panel);
+    		
+        //config windowcom
+        Dimension screenSize = Toolkit.getDefaultToolkit ( ).getScreenSize ( );
+        int x = ( screenSize.width - form.getWidth ( ) ) / 2;
+        int y = ( screenSize.height - form.getHeight ( ) ) / 2;
+		form.setVisible(true);
+        form.setLocation(x, y);
+		form.setResizable(false);
+            
+        //Hello Pokemon
+        System.out.println("Hello Pokemon");
+        System.out.println();
+    }
 }
